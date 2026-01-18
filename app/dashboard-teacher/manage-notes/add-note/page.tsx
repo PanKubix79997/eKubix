1d1f8ef47ccac2e98bfed379a8030efcdaf76070
Kuba <pankubix79997@Mac-mini-Kuba.local>
2026-01-18 12:26:42 +0100

    poprawiono blad z dodawaniem uwag.

--- app/dashboard-teacher/manage-notes/add-note/page.tsx
+++ app/dashboard-teacher/manage-notes/add-note/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+export const dynamic = "force-dynamic";
 
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
+
 
 type Student = {
   _id: string;
